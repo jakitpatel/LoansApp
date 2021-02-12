68c8f6bdb67576e70b9010e74b2d22f659da90eb
Jakit <jakit@Jakits-MacBook-Pro.local>
2021-02-12 17:44:40 +0530

    added status comments for external, added pagination for one page also, added teambmemebr on top

--- src/components/Dashboard/DashboardMain.js
+++ src/components/Dashboard/DashboardMain.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from 'react-redux';
 import preval from 'preval.macro';
+import { useDispatch } from 'react-redux';
 
 function DashboardMain(props) {
     const { name, isInternalUser, teamInt } = useSelector(state => {
@@ -8,10 +9,20 @@ function DashboardMain(props) {
             ...state.userReducer
         }
     });
+    const dispatch = useDispatch();
     console.log("name : "+name);
     let userType = "External User";
     if(isInternalUser){
         userType = "Internal User";
+    }
+    const onChangeTeam = (e) => {
+        console.log("On Change of Team Value");
+        dispatch({
+            type:'UPDATEUSER',
+            payload:{
+              teamInt: e.target.value
+            }
+          });
     }
     return (
         <React.Fragment>
@@ -22,6 +33,24 @@ function DashboardMain(props) {
                     <b>Build Version</b> :- {process.env.REACT_APP_VERSION} <br />
                     <b>Build Date</b> :- {preval`module.exports = new Date().toLocaleString();`}.
                 </div>
+                {isInternalUser &&
+                <>
+                <div style={{height:"40px"}} />
+                <div className="form-group">
+                    <label className="col-sm-2 col-form-label" style={{width:"90px"}}>Team</label>
+                    <select
+                        className="form-control custom-select col-sm-4"
+                        name="teamInt"
+                        value={teamInt}
+                        onChange={e => { onChangeTeam(e)}}
+                    >
+                        <option value="teama">teama</option>
+                        <option value="teamb">teamb</option>
+                        <option value="teamc">teamc</option>
+                    </select>
+                </div>
+                </>
+                }
             </div>
         </React.Fragment>
     );
